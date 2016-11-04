7de8a6cbac1cf393b147cc7451b7b7f0de45e64d
Andres <andres.viikmaa@gmail.com>
2016-11-04 13:58:51 +0200

    FC-Diploaf control App

---
+++ ControlCentrer/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace ControlCentrer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.pnlOnline = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged_1);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(210, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(608, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 446);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(453, 470);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Flip Field";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::ControlCentrer.Properties.Resources.field2;
+            this.panel1.Location = new System.Drawing.Point(210, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(608, 378);
+            this.panel1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pnlOnline);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(191, 106);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(16, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Tag = "1";
+            this.radioButton1.Text = "Master";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(16, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(52, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Tag = "0";
+            this.radioButton2.Text = "Slave";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // pnlOnline
+            // 
+            this.pnlOnline.BackColor = System.Drawing.Color.Red;
+            this.pnlOnline.Location = new System.Drawing.Point(110, 19);
+            this.pnlOnline.Name = "pnlOnline";
+            this.pnlOnline.Size = new System.Drawing.Size(37, 31);
+            this.pnlOnline.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 505);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Tag = "0";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Panel pnlOnline;
+
+    }
+}
+
---
+++ ControlCentrer/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlCentrer
+{
+    public partial class Form1 : Form
+    {
+        private bool closePending;
+
+        Pen myPen = new Pen(Color.Red);
+        Graphics formGraphics;
+        SolidBrush myBrush = new SolidBrush(System.Drawing.Color.Red);
+        int fieldX;
+        int fieldY;
+        Bitmap field1 = new Bitmap(Properties.Resources.field2);
+        Bitmap field2 = new Bitmap(Properties.Resources.field21);
+        bool bMaster = true;
+        bool restart = false;
+        bool online = false;
+        public Form1()
+        {
+            InitializeComponent();
+
+            backgroundWorker1.RunWorkerAsync(null);
+
+            formGraphics = this.panel1.CreateGraphics();
+            fieldX = 304;
+            fieldY = 202;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Loopback, 0);
+            using (var udpClient = new UdpClient(31000 + (bMaster ? 1 : 0)))
+            {
+                udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 1000);
+
+                while (!worker.CancellationPending)
+                {
+                    try
+                    {
+                        var buffer = udpClient.Receive(ref remoteEndPoint);
+                        var msg = System.Text.Encoding.ASCII.GetString(buffer);
+                        worker.ReportProgress(0, msg);
+                        if (!online)
+                        {
+                            online = true;
+                            pnlOnline.BackColor = Color.Green;
+                        }
+                    }
+                    catch (SocketException)
+                    {
+                        if (online)
+                        {
+                            online = false;
+                            pnlOnline.BackColor = Color.Red;
+                        }
+                        ;
+                    }
+                    // ...
+                }
+
+            }
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (backgroundWorker1.IsBusy)
+            {
+                closePending = true;
+                backgroundWorker1.CancelAsync();
+                e.Cancel = true;
+                this.Enabled = false;   // or this.Hide()
+                return;
+            }
+            base.OnFormClosing(e);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void backgroundWorker1_ProgressChanged_1(object sender, ProgressChangedEventArgs e)
+        {
+            bool flipped = Tag.ToString() == "1";
+            formGraphics.DrawImage(flipped ? field1 : field2, new Point(0,0));
+            //panel1.Invalidate();
+            string data = e.UserState as String;
+            var items = data.Split(" ".ToCharArray());
+            if (items[0] == "STT")
+            {
+                int ballCount = Int32.Parse(items[1]);
+                for (int i = 0; i < ballCount; i++)
+                {
+                    drawBall(new Random().Next(1, 30) + fieldX + Int32.Parse(items[i * 2 + 3]), fieldY + Int32.Parse(items[i * 2 + 2]));
+                }
+            }
+
+        }
+
+        private void drawBall(int p1, int p2)
+        {
+            //throw new NotImplementedException();
+            formGraphics.FillEllipse(myBrush, new Rectangle(p1, p2, 10, 10));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            formGraphics.FillEllipse(myBrush, new Rectangle(0, 0, 200, 300));
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (closePending) this.Close();
+            closePending = false;
+            if (restart)
+            {
+                backgroundWorker1.RunWorkerAsync(null);
+                restart = false;
+            }
+                
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool flipped = Tag.ToString() == "1";
+            Tag = flipped ? "0" : "1";
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            bMaster = (sender as RadioButton).Tag.ToString() == "1";
+            backgroundWorker1.CancelAsync();
+            restart = true;
+        }
+    }
+}
