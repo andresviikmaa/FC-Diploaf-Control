aed68908639a4690b7de8201f0dc0436c5d850fa
andresviikmaa <andres.viikmaa@gmail.com>
2016-11-16 09:04:30 +0200

    upd test

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 
+var PORT = 33333;
+var HOST = '127.0.0.1';
+
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+
+server.on('listening', function () {
+    var address = server.address();
+    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+
+server.on('message', function (message, remote) {
+    console.log(remote.address + ':' + remote.port + ' - ' + message);
+
+});
+
+server.bind(PORT, HOST);
+
 var app = express();
 
 // all environments
